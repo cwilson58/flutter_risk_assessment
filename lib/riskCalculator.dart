78c0c654249d9752fa0f8225b1b29038a16d0da5
Cameron <wilsoncameron42@gmail.com>
2022-07-22 19:12:48 -0600

    Moved appbar

--- lib/riskCalculator.dart
+++ lib/riskCalculator.dart
@@ -53,36 +53,7 @@ class _RiskState extends State<RiskCalculator> {
     double mainWindowHeight = appHeight * 0.8;
 
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        centerTitle: true,
-        leadingWidth: 600,
-        leading: const Text(
-          "Project Name",
-          style: TextStyle(
-            color: Color(0xFFFFFFFF),
-            fontSize: 48,
-          ),
-        ),
-        title: RichText(
-          text: const TextSpan(
-              text: "Total Risk:",
-              style: TextStyle(
-                color: Color(0xFFFFFFFF),
-                fontSize: 48,
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                    text: "Green",
-                    style: TextStyle(
-                      color: Color(0xFF01B100),
-                      fontSize: 48,
-                    ))
-              ]),
-        ),
-        backgroundColor: const Color(0xFF08006A),
-      ),
+ 
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
